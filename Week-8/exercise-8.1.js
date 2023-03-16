64c242b8529013f686cb5a78bae84ce4aa45f6c5
Sourav <1997mehra@gmail.com>
2023-03-16 18:28:17 +0530

    Updated Linked List logic with tail node

--- Week-8/exercise-8.1.js
+++ Week-8/exercise-8.1.js
@@ -18,6 +18,7 @@ class LinkedList {
    */
   constructor(...items) {
     this.head = null;
+    this.tail = null;
     this.size = 0;
 
     items.forEach((item) => {
@@ -26,12 +27,15 @@ class LinkedList {
       // If linked list is empty
       if (!this.head) {
         this.head = newNode;
+        this.tail = this.head;
       } else {
-        let lastNode = this.last();
+        let lastNode = this.tail;
         if (lastNode == null) {
           this.head = newNode;
+          this.tail = this.head;
         } else {
           lastNode.next = newNode;
+          this.tail = newNode;
         }
       }
 
@@ -41,7 +45,7 @@ class LinkedList {
 
   /**
    * Adds a new node to the linked list at the last position
-   * !Time Complexity: O(n)
+   * !Time Complexity: O(1) {O(1) here with tail pointer, else O(n) without tail}
    * !Space Complexity: O(1)
    * @param {number} val The value of the new that will be added
    */
@@ -51,35 +55,13 @@ class LinkedList {
     // If linked list is empty
     if (!this.head) {
       this.head = newNode;
+      this.tail = this.head;
     } else {
-      let temp = this.head;
-
-      // Iterating to the last node in the linked list
-      while (temp && temp.next != null) {
-        temp = temp.next;
-      }
-
       // Attaching the new node to the last node in the linked list
-      temp.next = newNode;
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
     this.size++;
-  }
-
-  /**
-   * Returns the last node in the linked list
-   * !Time Complexity: O(n)
-   * !Space Complexity: O(1)
-   * @return {number} The node at the end of the linked list
-   */
-  last() {
-    let temp = this.head;
-    if (this.head == null) {
-      return null;
-    }
-    while (temp && temp.next != null) {
-      temp = temp.next;
-    }
-    return temp;
   }
 
   /**
